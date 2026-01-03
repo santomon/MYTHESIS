a5b10b1f9c6f1d08141d51f205cd58b57e0c40ce
santomon <qa12_8@yahoo.de>
2026-01-03 15:04:19 +0100

    conceptual attempt applying correction

--- experiments/proton_density/correction_application.py
+++ experiments/proton_density/correction_application.py
@@ -1,4 +1,5 @@
 import json
+from copy import deepcopy
 
 import matplotlib.pyplot as plt
 import nibabel as nib
@@ -6,10 +7,10 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from utils import generate_gradient_map
 from env import env
-from utils import load_nifti_as_2d
+from utils import load_nifti_as_2d, generate_gradient_map
 
-IMAGE_PATH = env.proton_density_nifti_path
-MASK_PATH = env.proton_density_mask_nifti_path
+IMAGE_PATH = env.perfusion_test_nifti_path
+MASK_PATH = env.perfusion_test_mask_nifti_path
 PARAMS_PATH = env.linear_params_path
 
 # Load NIfTI files
@@ -21,5 +22,29 @@ mask = load_nifti_as_2d(MASK_PATH)
 with open(PARAMS_PATH, "r") as f:
     params = json.load(f)
 
+bin_mask = mask == 1
+
+mask_y, mask_x = np.where(bin_mask)
+center_x = np.mean(mask_x)
+center_y = np.mean(mask_y)
+
+shape = bin_mask.shape
 
 
+gradient_image = generate_gradient_map(shape, center_x=center_x, center_y=center_y, **params)
+
+gradient_image_non_zero_mask = gradient_image != 0
+
+image = image.astype(float)
+normalized_image = deepcopy(image)
+# normalized_image[gradient_image_non_zero_mask] = image[gradient_image_non_zero_mask] / gradient_image[gradient_image_non_zero_mask]
+normalized_image = image / gradient_image
+normalized_image[mask == 0] = 0
+
+
+
+
+print(normalized_image)
+
+plt.imshow(normalized_image)
+plt.show()
