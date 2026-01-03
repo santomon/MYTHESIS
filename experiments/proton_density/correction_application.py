1555429d5785f1c03fd8f813185b7e9fc69c7bf3
santomon <qa12_8@yahoo.de>
2026-01-03 14:44:59 +0100

    prepping with paths and shit

--- experiments/proton_density/correction_application.py
+++ experiments/proton_density/correction_application.py
@@ -1,17 +1,25 @@
-import pydicom
 import json
 
-PARAMS_PATH = "./data/linear_params.json"
-DICOM_PATH = r"./data/T1_CMR1001.5 T1_CMR1001.5/Anonymous Study/MR Dynamic Stress VP 120dyn SSFP_SAX_b1s/MR000022.dcm"  
+import matplotlib.pyplot as plt
+import nibabel as nib
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from utils import generate_gradient_map
+from env import env
+from utils import load_nifti_as_2d
+
+IMAGE_PATH = env.proton_density_nifti_path
+MASK_PATH = env.proton_density_mask_nifti_path
+PARAMS_PATH = env.linear_params_path
+
+# Load NIfTI files
+print("Loading image and mask...")
+
+image = load_nifti_as_2d(IMAGE_PATH)
+mask = load_nifti_as_2d(MASK_PATH)
 
 with open(PARAMS_PATH, "r") as f:
     params = json.load(f)
-
-dicom = pydicom.dcmread(DICOM_PATH)
-pixel_array = dicom.pixel_array
-
-
-
 
 
 
