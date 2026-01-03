0c28995f36bd6ba4adfe56e0c4c0b1f6b8867c04
santomon <qa12_8@yahoo.de>
2026-01-03 13:20:25 +0100

    adding the rest of the env variables

--- experiments/env.py
+++ experiments/env.py
@@ -7,6 +7,14 @@ load_dotenv()
 class Env(BaseSettings):
     proton_density_dicom_path: str
     proton_density_nifti_path: str
+    proton_density_mask_nifti_path: str
+
+    perfusion_test_dicom_path: str
+    perfusion_test_nifti_path: str
+    perfusion_test_mask_nifti_path: str
+
+
+    linear_params_path: str
 
 
 env = Env()
